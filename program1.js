469762f502e443495c001c1fbac1203567701d4e
Divyadeep25 <ydeep641@gmail.com>
2023-04-16 13:10:39 +0530

    traffic light

---
+++ program1.js
@@ -0,0 +1,15 @@
+let traffic_light= "red";
+
+if(traffic_light=="red"){
+  console.log("Stop");
+}
+else if(traffic_light=="yellow"){
+  console.log("Be ready");
+}
+else if(traffic_light=="green"){
+  console.log("GO");
+}
+else{
+  console.log("Error");
+}
+
